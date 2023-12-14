3e682dec12a40560114c4fc31febebad10385197
Jonathan Holtmann <1270177+jholtmann@users.noreply.github.com>
2023-12-13 16:10:36 -0800

    Implement day 2 solution

---
+++ aoc2023/src/day02.rs
@@ -0,0 +1,103 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    input_file: PathBuf
+}
+
+const RADIX: u32 = 10;
+
+fn read_int(line: &str) -> anyhow::Result<(usize, usize)> {
+    println!("{}", line);
+    let mut chars = String::from("");
+
+    let mut index = 0;
+    for (i, c) in line.chars().enumerate() {
+        if !c.is_digit(RADIX) {
+            index = i;
+            break;
+        }
+
+        chars.push(c);
+    }
+
+    Ok((chars.parse::<usize>()?, index))
+}
+
+fn main() -> anyhow::Result<()> {
+    let marble_limits: HashMap<&str, usize> = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+
+    let args = Args::parse();
+
+    let contents = fs::read_to_string(args.input_file)?;
+    let lines = contents.lines();
+
+    let mut solution = 0;
+
+    for line in lines {
+        let content = String::from(line);
+
+        // extract game number
+        let game_index = content.find(":").unwrap();
+        let game = &content[..game_index]
+            .replace("Game ", "")
+            .parse::<usize>()?;
+
+        // split game into component hands
+        let hands = &content[(game_index + 2)..].split("; ").collect::<Vec<&str>>();
+
+        // track if game is still valid
+        let mut allowed_game = true;
+        for hand in hands {
+            // extract marble counts from hand
+            let mut hand_marble_counts = HashMap::new();
+            for marble_data in &hand.split(", ").collect::<Vec<&str>>() {
+                let (marble_count, i) = read_int(marble_data)?;
+                let color = &marble_data[(i + 1)..];
+
+                if hand_marble_counts.contains_key(color) {
+                    *hand_marble_counts.get_mut(&color).unwrap() += marble_count;
+                } else {
+                    hand_marble_counts.insert(color, marble_count);
+                }
+            }
+
+            // test if hand is valid
+            for (color, marble_total) in hand_marble_counts {
+                if !marble_limits.contains_key(color) {
+                    panic!("invalid color {color}");
+                }
+
+                let limit = marble_limits[color];
+
+                if marble_total > limit {
+                    allowed_game = false;
+                    println!("game {game} {color} {marble_total} > {limit}");
+                    break;
+                }
+            }
+
+            // if a hand is invalid, there's no need to keep parsing the rest of the hands
+            if !allowed_game {
+                break;
+            }
+        }
+
+        if allowed_game {
+            solution += game;
+        }
+    }
+
+    println!("Result: {}", solution);
+
+    Ok(())
+}
